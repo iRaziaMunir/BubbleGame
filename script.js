a60d9cf45c3b75b19e25aba708d416ca875b2c19
iRaziMunir <80644602+iRaziaMunir@users.noreply.github.com>
2023-12-18 15:15:24 +0500

    .

--- script.js
+++ script.js
@@ -62,3 +62,11 @@ function timerFun() {
     timerFun();
     getNewHit();  
     makebubbles();
+
+    let a =[1,2,3];
+    let b = a;
+    
+    b.push(4);
+
+    console.log(a);
+    console.log(b);
